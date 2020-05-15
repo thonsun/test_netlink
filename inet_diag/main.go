e2f60faf6f6de0c61997376974ff3fb179ab2cdb
tangsheng.thonsun <tangsheng.thonsun@bytedance.com>
2020-05-15 18:21:05 +0800

    netlink_inet_diag test
    Change-Id: Ibc75b4eb41d8176cca4ba965f9be4c8cb2836a39

--- inet_diag/main.go
+++ inet_diag/main.go
@@ -31,9 +31,9 @@ func main() {
 		sdiag_family:syscall.AF_INET,
 		sdiag_protocol:syscall.IPPROTO_TCP,
 		// 特定的ip port socket inode 查询
-		//idiag_stats:TCPF_ALL &^((1<<TCP_SYN_RECV) | (1<<TCP_TIME_WAIT) | (1<<TCP_CLOSE)),
+		idiag_stats:TCPF_ALL &^((1<<TCP_SYN_RECV) | (1<<TCP_TIME_WAIT) | (1<<TCP_CLOSE)),
 		//sdiag_ext: (1<<(INET_DIAG_INFO-1)),
-		id: inet_diag_sockid{idiag_src:src,idiag_sport:sport,idiag_dst:dst,idiag_dport:dport},
+		//id: inet_diag_sockid{idiag_src:src,idiag_sport:sport,idiag_dst:dst,idiag_dport:dport},
 	}
 	data, err := conn_req.marshalBinary()
 	if err != nil{
@@ -51,11 +51,11 @@ func main() {
 	if err != nil {
 		log.Printf("send req msg error:%v",err)
 	}
-	fmt.Printf("send status:%#v\n",msgs)
+	fmt.Printf("send status:%v\n",msgs)
 
 	msg, err := c.Receive()
 	if err != nil {
 		fmt.Printf("recieve msg error:%#v\n",err)
 	}
-	fmt.Printf("recieve msg:%#v\n",msg)
+	fmt.Printf("recieve msg:%v\n",msg)
 }
